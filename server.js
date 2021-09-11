b5af6c884920d82e503e1acac10135d7c2aff148
Zachary Gomez <zgomez301@gmail.com>
2021-09-11 13:36:38 -0700

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -31,6 +31,17 @@ app.get('/tip/:total/:tipPercentage', (req,res) => {
 //Magic 8 Ball
 //////////////////////////
 
+const eightBitReponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+
+app.get('/magic/:question', (req,res) => {
+    let question = req.params.question
+    //bitwise operator - faster for under 16b but has problems with neg
+    let response = eightBitReponse[~~(Math.random() * eightBitReponse.length)]
+    res.send(`<i>"${question}"</i><br><h1>${response}</h1>`)
+    
+})
+
 app.listen(port, () => {
     console.log("Express server is listening on port", port)
 })
