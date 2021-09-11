9f1a5470855c05d5fe574298441a2abf07e4d216
Zachary Gomez <zgomez301@gmail.com>
2021-09-11 13:09:36 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ app.get('/greeting/:name', (req, res) => {
     res.send('<i>Hello there.</i><br>' + req.params.name + ', you are a bold one!');
 });
 
+//////////////////////////
+//Tip Calculator
+//////////////////////////
+
+app.get('/tip/:total/:tipPercentage', (req,res) => {
+    const total = Number(req.params.total);
+    const tipPercentage = Number(req.params.tipPercentage) * 0.01;
+    const final = total * tipPercentage
+    console.log(final)
+    res.send(`You owe a total of ${final} smackaroos`)
+})
+
+//////////////////////////
+//Magic 8 Ball
+//////////////////////////
+
 app.listen(port, () => {
     console.log("Express server is listening on port", port)
 })
